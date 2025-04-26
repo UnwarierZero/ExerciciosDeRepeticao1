284aac21c4e64a4a04ecf73f975cc19c7e7518c2
Caio Alexandre <caaio2412@gmail.com>
2025-04-26 18:49:28 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i =0;
+
+        while (i<50){
+            i=i+1;
+            System.out.println(i);
+        }
+    }
+}
